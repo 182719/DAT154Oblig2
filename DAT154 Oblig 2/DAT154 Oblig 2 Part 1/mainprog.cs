4dc0b7a4f28457ec79f6277d37c11b3b860b76e2
eirikalv <h@g.gov>
2020-02-06 09:00:33 +0100

    fikset litt i spaceobj + begynt litt på task 4

--- DAT154 Oblig 2/DAT154 Oblig 2 Part 1/mainprog.cs
+++ DAT154 Oblig 2/DAT154 Oblig 2 Part 1/mainprog.cs
@@ -6,19 +6,12 @@ class Astronomy {
 	public static void Main() {
 		List<SpaceObject> solarSystem = new List<SpaceObject>
 		{
-		new Star("Sun"),
-		new Planet("Mercury"),
-		new Planet("Venus"),
-		new Planet("Terra"),
-		new Moon("The Moon"),
-		new DwarfPlanet("The dwarf"),
-		new Comet("The Comet"),
-		new AsteroidBelt("The Asteroid belt"),
-		new Asteroid("The Asteroid")
+			new Planet("Mercury", "Darkgrey", 2439, 57910, 2112, 1416, 47)
 		};
 
 		foreach (SpaceObject obj in solarSystem) {
 			obj.Draw();
+			
 		}
 
 		Console.ReadLine();
--- DAT154 Oblig 2/DAT154 Oblig 2 Part 1/spaceobj.cs
+++ DAT154 Oblig 2/DAT154 Oblig 2 Part 1/spaceobj.cs
@@ -6,9 +6,9 @@ namespace SpaceSim
     {
         public String name { get; set; }
         public String color { get; set; }
-        public int radius { get; set; } 
+        public int radius { get; set; } //represented i km
 
-        public SpaceObject(String name)
+        public SpaceObject(String name, String color, int radius)
         {
             this.name = name;
         }
@@ -21,11 +21,11 @@ namespace SpaceSim
 
     public class Star : SpaceObject
     {
-        public int orbitalRadius { get; set; }
-        public int orbitalPeriod { get; set; }
-        public int rotationalPeriod { get; set; }
+        public int orbitalRadius { get; set; } //represented in (000 km)
+        public int orbitalPeriod { get; set; } //represented in hours
+        public int rotationalPeriod { get; set; } // represented in hours
 
-        public Star(String name) : base(name) { }
+        public Star(String name, String color, int radius) : base(name, color, radius) { }
         public override void Draw()
         {
             Console.Write("Star : ");
@@ -35,14 +35,15 @@ namespace SpaceSim
 
     public class Planet : SpaceObject
     {
-        public int orbitalRadius { get; set; }
-        public int orbitalPeriod { get; set; }
-        public int orbitalSpeed { get; set; }
-        public int rotationalPeriod { get; set; }
+        public int orbitalRadius { get; set; } // represented in (000 km)
+        public int orbitalPeriod { get; set; } //represented in hours
+        public int orbitalSpeed { get; set; } // represented in km/s
+        public int rotationalPeriod { get; set; } // represented in hours
         public double[] position { get; set; } = new double[2];
 
 
-        public Planet(String name) : base(name) { }
+        public Planet(String name, String color, int radius, int orbitalRadius, int orbitalPeriod, int rotationalPeriod, int orbitalSpeed) 
+            : base(name, color, radius) { }
         public override void Draw()
         {
             Console.Write("Planet: ");
@@ -60,7 +61,8 @@ namespace SpaceSim
     public class Moon : Planet
     {
         public Planet planet { get; set; }
-        public Moon(String name) : base(name) { }
+        public Moon(String name, String color, int radius, int orbitalRadius, int orbitalPeriod, int rotationalPeriod, int orbitalSpeed)
+            : base(name, color, radius, orbitalRadius, orbitalPeriod, rotationalPeriod, orbitalSpeed) { }
         public override void Draw()
         {
             Console.Write("Moon : ");
@@ -76,7 +78,8 @@ namespace SpaceSim
     }
     public class DwarfPlanet : Planet
     {
-        public DwarfPlanet(String name) : base(name) { }
+        public DwarfPlanet(String name, String color, int radius, int orbitalRadius, int orbitalPeriod, int rotationalPeriod, int orbitalSpeed)
+            : base(name, color, radius, orbitalRadius, orbitalPeriod, rotationalPeriod, orbitalSpeed) { }
         public override void Draw()
         {
             Console.Write("Dwarf planet : ");
@@ -91,7 +94,7 @@ namespace SpaceSim
         public int orbitalPeriod { get; set; }
         public int rotationalPeriod { get; set; }
 
-        public Comet(String name) : base(name) { }
+        public Comet(String name, String color, int radius) : base(name, color, radius) { }
         public override void Draw()
         {
             Console.Write("Comet: ");
@@ -100,7 +103,7 @@ namespace SpaceSim
     }
     public class AsteroidBelt : SpaceObject
     {
-        public AsteroidBelt(String name) :base(name) { }
+        public AsteroidBelt(String name, String color, int radius) : base(name, color, radius) { }
         public override void Draw()
         {
             Console.Write("Asteroid belt : ");
@@ -109,7 +112,8 @@ namespace SpaceSim
     }
     public class Asteroid : Planet
     {
-        public Asteroid(String name) : base(name) { }
+        public Asteroid(String name, String color, int radius, int orbitalRadius, int orbitalPeriod, int rotationalPeriod, int orbitalSpeed)
+            : base(name, color, radius, orbitalRadius, orbitalPeriod, rotationalPeriod, orbitalSpeed) { }
         public override void Draw()
         {
             Console.Write("Asteroid : ");
