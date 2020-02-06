d6b85f063f9618f7c7a04d7bed2fd9a16934cc07
182719 <joakim-a@hotmail.com>
2020-02-06 13:45:05 +0100

    Lagt til planeter. Konstruktører er feil.

--- DAT154 Oblig 2/DAT154 Oblig 2 Part 1/mainprog.cs
+++ DAT154 Oblig 2/DAT154 Oblig 2 Part 1/mainprog.cs
@@ -6,7 +6,19 @@ class Astronomy {
 	public static void Main() {
 		List<SpaceObject> solarSystem = new List<SpaceObject>
 		{
-			new Planet("Mercury", "Darkgrey", 2439, 57910, 2112, 1416, 47)
+			new Planet("Mercury", "Darkgrey", 2439, 57910, 88 * 24, 1416, 47),
+			new Planet("Venus", "Beige", 6051, 108200, 225 * 24, 243, 35),
+			new Planet("Earth", "Blue", 6371, 149600, 365 * 24, 24, 30),
+			new Planet("Mars", "Red", 3389, 227940, 687 * 24, 25, 24),
+			new Planet("Jupiter", "Beige Red", 69911, 778330, 4333, 10, 13),
+			new Planet("Saturn", "Eggshell", 58232, 1429400, 10760, 11, 10),
+			new Planet("Uranus", "Pale blue", 25362, 2870990, 30685, 17, 7),
+			new Planet("Neptune", "Blue", 24622, 4504300, 60190, 16, 5),
+			new DwarfPlanet("Pluto", "Steel Red", 1188, 5913520, 90550, 6387 * 24, 4743),
+			new AsteroidBelt("The asteroid belt", "Grey Rock", 69420),
+			new Asteroid("243 Ida", "Grey", 16, 4280, 1767, 5, 420)
+
+
 		};
 
 		foreach (SpaceObject obj in solarSystem) {
