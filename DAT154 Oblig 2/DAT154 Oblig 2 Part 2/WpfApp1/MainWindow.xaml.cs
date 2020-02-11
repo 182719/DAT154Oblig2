35adb26332d13a4c5ea38a8da0020730abaa7c3e
Joakim Johesan <182719@stud.hvl.no>
2020-02-11 21:23:17 +0100

    DrawSolarSystem now iteratively draws the solar system components, still needs a lot of work, but atm. it produces something legible.

--- DAT154 Oblig 2/DAT154 Oblig 2 Part 2/WpfApp1/MainWindow.xaml.cs
+++ DAT154 Oblig 2/DAT154 Oblig 2 Part 2/WpfApp1/MainWindow.xaml.cs
@@ -32,42 +32,56 @@ namespace WpfApp1
             List<SpaceObject> solarSystem = solar_system.getSolarSystem();
             //drawSolarSystem(solarSystem);
 
-            this.Loaded += (s,o) => drawSolarSystem(solarSystem);
+            this.Loaded += (s,o) => drawSolarSystem(solarSystem, sun);
 
         }
 
 
-        public void drawSolarSystem(List<SpaceObject> solar_system)
+        public void drawSolarSystem(List<SpaceObject> solar_system, Star sun)
         {
 
-            // Create a red Ellipse.
-            Ellipse myEllipse = new Ellipse();
+            //TODO: DRAW SUN:
 
-            // Create a SolidColorBrush with a red color to fill the 
-            // Ellipse with.
-            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            //TODO: DRAW all sub-objects in solar_system
 
-            // Describes the brush's color using RGB values. 
-            // Each value has a range of 0-255.
-            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
-            myEllipse.Fill = mySolidColorBrush;
-            myEllipse.StrokeThickness = 2;
-            myEllipse.Stroke = Brushes.Black;
+            double screenOffsetX = myCanvas.RenderSize.Width / 2;
+            double screenOffsetY = myCanvas.RenderSize.Height / 2;
+            foreach (SpaceObject obj in solar_system) {
 
-            // Set the width and height of the Ellipse.
-            myEllipse.Width = 200;
-            myEllipse.Height = 100;
+                obj.updatePosition(100, sun);
+                
+                //Getting information from obj
+                string type = obj.GetType().Name;
 
-            // Add the Ellipse to the StackPanel.
+                //Transforming to screenSpacePositon
+                double[] screenPos = transformSpacePosToScreenPos(obj.position, screenOffsetX, screenOffsetY);
 
-            double x = myCanvas.RenderSize.Width / 2 - myEllipse.Width / 2;
-            double y = myCanvas.RenderSize.Height / 2;
-            myCanvas.Children.Add(myEllipse);
+                //Rendering information about object to screen
+                TextBox textBox = new TextBox();
+                textBox.Text = type + ": "+ obj.name;
+                myCanvas.Children.Add(textBox);
+                Canvas.SetLeft(textBox, screenPos[0]);
+                Canvas.SetTop(textBox, screenPos[1]);
+                Ellipse ellipse = new Ellipse();
+                SolidColorBrush solidColorBrush = new SolidColorBrush();
+                solidColorBrush.Color = Color.FromArgb(255, 255, 0, 1);
+                ellipse.Fill = solidColorBrush;
+                ellipse.StrokeThickness = 2;
+                ellipse.Stroke = Brushes.Black;
+                ellipse.Width = obj.radius / 10000;
+                ellipse.Height = obj.radius / 10000;
+                myCanvas.Children.Add(ellipse);
+                Canvas.SetLeft(ellipse, screenPos[0]);
+                Canvas.SetTop(ellipse, screenPos[1]);
 
-            
-            Canvas.SetLeft(myEllipse, x);
 
-            
+            }
+        }
+
+        private double[] transformSpacePosToScreenPos(double[] position, double sOX, double sOY)
+        {
+            double[] screenPos = { sOX + position[0] / 10000, sOY + position[1] / 10000 };
+            return screenPos;
         }
     }
 }
