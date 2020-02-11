82a6fa69b682766bbdbbfa0cd63f998fdc96dbba
182719 <joakim-a@hotmail.com>
2020-02-11 14:38:11 +0100

    Part 2.

---
+++ DAT154 Oblig 2/DAT154 Oblig 2 Part 2/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SpaceSim;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+			Star sun = new Star("Sun", "Yellow", 1390000, 0, 0, (27 * 24));
+			sun.position[0] = 0;
+			sun.position[1] = 0;
+
+            List<SpaceObject> solarSystem = SolarSystem.getSolarSystem(sun);
+		}
+    }
+}
