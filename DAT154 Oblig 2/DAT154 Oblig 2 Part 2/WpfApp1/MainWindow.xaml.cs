29d5e6ddaf7ac4d8237a65c2fdfb245552fdc779
Eirik <180339@stud.hvl.no>
2020-02-11 15:52:29 +0100

    ellipse

--- DAT154 Oblig 2/DAT154 Oblig 2 Part 2/WpfApp1/MainWindow.xaml.cs
+++ DAT154 Oblig 2/DAT154 Oblig 2 Part 2/WpfApp1/MainWindow.xaml.cs
@@ -30,12 +30,44 @@ namespace WpfApp1
 			sun.position[1] = 0;
             SolarSystem solar_system = new SolarSystem(sun);
             List<SpaceObject> solarSystem = solar_system.getSolarSystem();
-		}
+            //drawSolarSystem(solarSystem);
+
+            this.Loaded += (s,o) => drawSolarSystem(solarSystem);
+
+        }
 
 
         public void drawSolarSystem(List<SpaceObject> solar_system)
         {
 
+            // Create a red Ellipse.
+            Ellipse myEllipse = new Ellipse();
+
+            // Create a SolidColorBrush with a red color to fill the 
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values. 
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.Black;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = 200;
+            myEllipse.Height = 100;
+
+            // Add the Ellipse to the StackPanel.
+
+            double x = myCanvas.RenderSize.Width / 2 - myEllipse.Width / 2;
+            double y = myCanvas.RenderSize.Height / 2;
+            myCanvas.Children.Add(myEllipse);
+
+            
+            Canvas.SetLeft(myEllipse, x);
+
+            
         }
     }
 }
